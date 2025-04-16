55e6c512d7c8dd53d9d22516c3cad92dfa3fc324
Joao-Fernandes59 <ajfo09@gmail.com>
2025-04-16 19:56:57 -0300

    Inicializa repositório com estrutura de Scripts em Python

---
+++ Calculadora/cores.py
@@ -0,0 +1,16 @@
+#! Cores utilizadas no projerto Calculadora em CONSTANTES
+
+#! Cores principais
+BRANCO = "#feffff"  # Branco puro
+AZUL_CLARO = "#6f9fbd"  # Azul claro acinzentado
+AZUL_ESCURO = "#38576b"  # Azul escuro petróleo
+
+#! Fundo e tons neutros
+FUNDO = "#3b3b3b"  # Cinza escuro
+CINZA_CLARO = "#ECEFF1"  # Cinza muito claro
+
+#! Cores de destaque
+LARANJA = "#FFAB40"  # Laranja vibrante
+VERMELHO = "#ff333a"  # Vermelho vivo
+VERDE = "#6bd66f"  # Verde claro e fresco
+DOURADO = "#ab8918"  # Dourado escuro / amarelo queimado
